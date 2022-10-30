a8fa0715e1249b9204fb7772a486aba33687e71f
tool3 <talhayut.dev@gmail.com>
2022-10-30 14:00:58 +0200

    fix: tiny refactor

--- commands/list.js
+++ commands/list.js
@@ -14,31 +14,31 @@ async function list(argv) {
           mid: '',
           'left-mid': '',
           'mid-mid': '',
-          'right-mid': '',
-        },
+          'right-mid': ''
+        }
       }
     : undefined;
-  
-  const tableOptions = { style: { head: [], border: [] }, ...chars  };
+
+  const tableOptions = { style: { head: [], border: [] }, ...chars };
   if (argv.truncate) {
-    tableOptions.colWidths = [12, 50, 33, 17, 30] 
+    tableOptions.colWidths = [12, 50, 33, 17, 30];
   }
   const t = new table(tableOptions);
   t.push([
-    { content: color('SHA', 'whiteBold'), hAlign: 'center' },
+    { content: color('SHA', 'whiteBold'), hAlign: 'center', },
     { content: color('MESSAGE', 'whiteBold'), hAlign: 'center' },
     { content: color('DATE', 'whiteBold'), hAlign: 'center' },
     { content: color('AUTHOR', 'whiteBold'), hAlign: 'center' },
-    { content: color('EMAIL', 'whiteBold'), hAlign: 'center' },
+    { content: color('EMAIL', 'whiteBold'), hAlign: 'center' }
   ]);
-  for (commit of commits) {
+  for (const commit of commits) {
     const formattedCommit = commit.split('  ');
     t.push([
-      { content: color(formattedCommit[0], 'blue'), hAlign: 'center'},
-      {content: color(formattedCommit[1], 'cyan') },
-      {content: color(formattedCommit[2], 'magenta') },
+      { content: color(formattedCommit[0], 'blue'), hAlign: 'center' },
+      { content: color(formattedCommit[1], 'cyan') },
+      { content: color(formattedCommit[2], 'magenta') },
       { content: color(formattedCommit[3], 'yellow'), hAlign: 'center' },
-      { content: color(formattedCommit[4], 'green'), hAlign: 'center' },
+      { content: color(formattedCommit[4], 'green'), hAlign: 'center' }
     ]);
   }
   console.log(t.toString());
